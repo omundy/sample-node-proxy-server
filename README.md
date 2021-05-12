3168d20fb50892fa13a547e0d8863794881c1c3f
Owen Mundy <omundy@gmail.com>
2021-05-12 10:21:05 -0400

    Update fetch

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 
 # Sample Node Proxy Server
 
-- How to use Node to create a proxy server to avoid CORS issues on the frontend
-- An example node/express app with frontend and backend Javascript
-- An example Heroku app (with Procfile!)
+
+Demonstrates:
+
+- How to use Node/Express to create a proxy server to avoid CORS issues in frontend Javascript
+- How to use fetch (frontend JS) and node-fetch (backend JS)
+- An example Node/Express app using frontend and backend Javascript
+- An example Node/Express app that runs on Heroku (requirements: Procfile, export app, etc.)
 
 
 
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,14 +4,22 @@
  * 	- on localhost => only #2 will succeed - #1 will be a different origin than localhost
  */
 
-// #1
+// fetch #1
 fetch('https://sample-node-proxy-server.herokuapp.com/proxy/satellites')
 	.then(d => d.text())
-	.then(d => console.log(d))
+	.then(str => {
+		// console.log(str); // text string
+		let json = JSON.parse(str); // convert to JSON
+		console.log(`fetch #2 => json.info.satcount = ${json.info.satcount}`); // log # satellites
+	})
 	.catch(err => console.error("fetch #1", err));
 
-// #2
+// fetch #2
 fetch('/proxy/satellites')
 	.then(d => d.text())
-	.then(d => console.log(d))
+	.then(str => {
+		// console.log(str); // text string
+		let json = JSON.parse(str); // convert to JSON
+		console.log(`fetch #2 => json.info.satcount = ${json.info.satcount}`); // log # satellites
+	})
 	.catch(err => console.error("fetch #2", err));
